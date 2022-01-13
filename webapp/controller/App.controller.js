ae885b484077fdf4d909e83689f999b729b3682e
Pratyush-BITS <2021sp93070@wilp.bits-pilani.ac.in>
2022-01-13 20:25:04 +0530

    step 7

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,25 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller,MessageToast) {
+	function (Controller,MessageToast,JSONModel) {
 		"use strict";
 
 		return Controller.extend("com.learn.walkthrough.controller.App", {
 			onInit: function () {
+
+				var oData = {
+					recipient : {
+						name : "World"
+					}
+				};
+
+				var oModel = new JSONModel(oData);
+				this.getView().setModel(oModel);
 
 			},
 			onPress: function(){

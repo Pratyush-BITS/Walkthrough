6811c0f2167be0cccc1cd4b9a1c5e10adad57024
Pratyush-BITS <2021sp93070@wilp.bits-pilani.ac.in>
2022-01-14 00:38:48 +0530

    step 20 - Data Types

---
+++ webapp/controller/InvoiceList.controller.js
@@ -0,0 +1,24 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller,MessageToast,JSONModel) {
+		"use strict";
+
+		return Controller.extend("com.learn.walkthrough.controller.InvoiceList", {
+			onInit: function(){
+				var oCurrencyModel= new JSONModel({
+                    currency: "INR"
+                });
+                this.getView().setModel(oCurrencyModel, "currency");
+               
+			},
+            onClick: function(){
+                MessageToast.show("hey");
+            }			
+		});
+	});

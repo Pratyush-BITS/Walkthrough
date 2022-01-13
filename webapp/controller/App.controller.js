afe840449832194f55f355eee4cfa92a85b4ed79
Pratyush-BITS <2021sp93070@wilp.bits-pilani.ac.in>
2022-01-13 20:29:21 +0530

    step 8

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
-	"sap/ui/model/json/JSONModel"
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller,MessageToast,JSONModel) {
+	function (Controller,MessageToast,JSONModel,ResourceModel) {
 		"use strict";
 
 		return Controller.extend("com.learn.walkthrough.controller.App", {
@@ -17,13 +18,20 @@ sap.ui.define([
 						name : "World"
 					}
 				};
-
 				var oModel = new JSONModel(oData);
 				this.getView().setModel(oModel);
 
+				var i18nModel = new ResourceModel({
+					bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
+				});
+				this.getView().setModel(i18nModel, "i18n");
 			},
 			onPress: function(){
-				MessageToast.show("Haai");
+				var oBundle = this.getView().getModel("i18n").getResourceBundle();
+				var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+				var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+				
+				MessageToast.show(sMsg);
 			}
 		});
 	});

53c4fc63d7c91d0264193d7a3ada3f9503bb017d
Pratyush-BITS <2021sp93070@wilp.bits-pilani.ac.in>
2022-01-14 11:51:10 +0530

    step 22 - custom formatter

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,15 +1,19 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
-	"sap/ui/model/json/JSONModel"
+	"sap/ui/model/json/JSONModel",
+	"../model/Formatter"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller,MessageToast,JSONModel) {
+	function (Controller,MessageToast,JSONModel,Formatter) {
 		"use strict";
 
 		return Controller.extend("com.learn.walkthrough.controller.InvoiceList", {
+			
+			formatter: Formatter,
+
 			onInit: function(){
 				var oCurrencyModel= new JSONModel({
                     INR: "₹"

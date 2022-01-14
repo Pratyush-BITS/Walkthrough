0ea0ad62df12fc03b8540075b12bd8b5b01578a5
Pratyush-BITS <2021sp93070@wilp.bits-pilani.ac.in>
2022-01-14 14:34:45 +0530

    Fix for Live text update & Filter along with step 24 - Sorting & Grouping

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -9,7 +9,7 @@ sap.ui.define([
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller,MessageToast,JSONModel,Formatter) {
+	function (Controller,MessageToast,JSONModel,Formatter,Filter,FilterOperator) {
 		"use strict";
 
 		return Controller.extend("com.learn.walkthrough.controller.InvoiceList", {
@@ -21,26 +21,27 @@ sap.ui.define([
                     INR: "₹"
                 });
                 this.getView().setModel(oCurrencyModel, "currency");
-               
+				this.oSF = this.byId("searchField");
 			},
             onClick: function(){
                 MessageToast.show("hey");
             },
 			onFilterInvoices: function(oEvent){
 				
+				//filter array, it can have multiple filters
 				var aFilter = [];
-				var sQuery = oEvent.getParamenter("query");
+				var sQuery = oEvent.getParameter('query');
 				
 				// Read about Filter & Filter operator :
 				// Filter -> https://sapui5.hana.ondemand.com/#/api/sap.ui.model.Filter
 				// FilterOperator -> https://sapui5.hana.ondemand.com/#/api/sap.ui.model.FilterOperator
 				if(sQuery){
-					aFilter.push(new Filter("ProductName", FilterOperator.StartsWith))
+					aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery))
 				}
 
 				var oList = this.byId("invoiceList");
 				var oBinding = oList.getBinding('items');
-				oBinding.Filter(aFilter)
+				oBinding.filter(aFilter)
 
 			}	
 		});
